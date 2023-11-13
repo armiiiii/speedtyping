3edf52d85de4f0f1c5342d7f4d3689adf40f744f
Igor Glushko <superigorek74@gmail.com>
2023-11-13 21:21:08 +0500

    Little preparations

--- backend/conf/typingspeed/views.py
+++ backend/conf/typingspeed/views.py
@@ -1,3 +1,13 @@
+import json
+import random
+
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+
+
+def view(request):
+    url = "https://www.mit.edu/~ecprice/wordlist.10000"
+    resp = requests.get(url)
+    words = resp.content.splitlines()
+    return json.loads(' '.join(random.choices(words)))
